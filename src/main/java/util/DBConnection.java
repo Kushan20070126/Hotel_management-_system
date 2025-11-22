34169c2431c63095e8008d575369a109ee174d08
Kushan Randika Herath <KUCSE242F-024@student.nibm.lk>
2025-11-22 16:11:54 +0530

    create DB Connection

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package util;
 
-/**
- *
- * @author KushanDev
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+
 public class DBConnection {
     
+    private Connection con;
+    private  static DBConnection instance;
+   
+    
+        private DBConnection(){
+          try{
+               
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel_db?useSSL=false","root","");
+            System.out.println("DataBase Connected !!");
+                
+                
+            }catch(Exception e){
+                
+                System.err.println("Database Connection Failed !!");
+                
+            }
+        }
+        
+         public static synchronized DBConnection getInstance(){
+             if(instance == null){
+                 instance = new DBConnection();
+             }
+             return  instance;
+         }
+           
+         public synchronized Connection getConnection(String query){
+             return  con;
+         }
+        
+         
+                   
 }
